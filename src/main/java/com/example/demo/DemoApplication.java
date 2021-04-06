8f3315846b690491029e87b330d015c4a6708ea9
olena.mordas <olena.mordas@synpulse.com>
2021-04-06 13:21:29 +0200

    same thing using Spring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SortAlgorithm sortAlgorithm = new QuickSortAlgorithmImpl();
-		BinarySearchImpl binarySearch = new BinarySearchImpl(sortAlgorithm);
+//		SortAlgorithm sortAlgorithm = new QuickSortAlgorithmImpl();
+//		BinarySearchImpl binarySearch = new BinarySearchImpl(sortAlgorithm);
+
+		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int res = binarySearch.binarySearch(new int[]{5, 7, 3}, 3);
 		System.out.println(res);
 
-		SpringApplication.run(DemoApplication.class, args);
 	}
 }
--- src/main/java/com/example/demo/QuickSortAlgorithmImpl.java
+++ src/main/java/com/example/demo/QuickSortAlgorithmImpl.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuickSortAlgorithmImpl implements SortAlgorithm {
 
     public int[] sort(int[] numbers) {
