46cb745565399cd80df13c55c7c31f7f50463a2d
olena.mordas <olena.mordas@synpulse.com>
2021-04-07 15:31:15 +0200

    removed unnecessary annotations

--- src/main/java/com/example/demo/XmlContextApplication.java
+++ src/main/java/com/example/demo/XmlContextApplication.java
@@ -3,12 +3,8 @@ package com.example.demo;
 import com.example.demo.xml.XMLPersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class XmlContextApplication {
 
 	private static Logger logger = LoggerFactory.getLogger(XmlContextApplication.class);
