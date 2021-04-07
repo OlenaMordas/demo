68d300923249503fec1467ef0af599394bd77f0e
olena.mordas <olena.mordas@synpulse.com>
2021-04-07 11:53:20 +0200

    @PostConstruct @PreDestroy

--- src/main/java/com/example/demo/basic/BinarySearchImpl.java
+++ src/main/java/com/example/demo/basic/BinarySearchImpl.java
@@ -1,12 +1,19 @@
 package com.example.demo.basic;
 
 import com.example.demo.SortAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class BinarySearchImpl {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("quick")
@@ -18,5 +25,15 @@ public class BinarySearchImpl {
         int[] sortedNums = sortAlgorithm.sort(numbers);
         // search array
         return 3;
+    }
+
+    @PostConstruct
+    public void postCostruct() {
+        logger.info("POST CONSTRUCT");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        logger.info("PRE DESTROY");
     }
 }
